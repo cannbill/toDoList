9f16b3dfad798cb912a88392a317664cdb0ab766
Username <bill.cann27@gmail.com>
2022-02-15 11:34:54 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props){
+    super(props);
+
+    this.state={
+      newTask:"",
+      list:[]
+    }
+  }
+
+  updateInput(key, value){
+    this.setState({
+      [key]: value
+    })
+  }
+
+  addTask(){
+    const newTask={
+      id: 1 + Math.random(),
+      value: this.state.newTask.slice()
+    };
+    const list = [...this.state.list];
+    list.push(newTask);
+    this.setState({
+      list,
+      newTask:""
+    });
+  }
+
+  deleteTask(id){
+    const list = [...this.state.list];
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({list: updatedList});
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div>
+          To do list:
+          <input
+          type="text"
+          placeholder="Type task here"
+          value={this.state.newTask}
+          onChange={e => this.updateInput("newTask", e.target.value)}
+          />
+          <button onClick={() => this.addTask()}>Add</button>
+          <ul>
+            {this.state.list.map(item => {
+              return(
+                <li key={item.id}>
+                  {item.value}
+                  <button onClick={() => this.delete.item(item.id)}>X</button>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders h2 element', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/To Do List/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe ("submit button", () => {
+  test("submit button is rendered", () => {
+    render(<App />);
+    const button = screen.getByText(/Add to list/i)
+    expect(button).toBeInTheDocument();
+  })
+})
+
+describe ("submit button", () => {
+  test("submit button is rendered", () => {
+    render(<App />);
+    const button = screen.getByText("Add to list")
+    fireEvent.click(button)
+    expect(button).toBeInTheDocument();
+
+    // const deleteButton = screen.getByText(/delete/i) this tests to see if the delete button is in the app.
+    // expect (deleteButton).toBeInTheDocument()
+  })
+
+  describe ("input element", () => {
+    test("input element is rendered", () => {
+      render(<App />);
+      const input = screen.getByRole("form")
+      fireEvent.change(input, { target: { value: "exercise"}})
+      expect(input.value).toBe("exercise");
+    }
